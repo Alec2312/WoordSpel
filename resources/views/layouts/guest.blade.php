cdc48cc0208d96543d72bbf33dfd406465eaaf03
A <1207074@student.roc-nijmegen.nl>
2025-05-27 13:39:04 +0200

    Created home page and added form styling

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="relative h-screen overflow-hidden">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<div class="flex h-full w-full absolute inset-0">
+    <div class="w-1/2 bg-[#FD4D4B]"></div>
+    <div class="w-1/2 bg-[#00BBFC]"></div>
+</div>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<div class="absolute inset-0 flex items-center justify-center pointer-events-none">
+    @guest
+        <img src="/storage/img/welkom.png" alt="Welkom afbeelding"
+             class="h-full object-contain"/>
+    @else
+        <img src="/storage/img/vs.png" alt="Versus afbeelding"
+             class="h-full object-contain"/>
+    @endguest
+</div>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+<div class="relative z-10 h-full w-full">
+    {{ $slot }}
+</div>
+
+</body>
 </html>
